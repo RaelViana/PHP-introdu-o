00bf6d0cefb9cfeea8e210b37cf4adfdda28ec7e
RaelViana <catedralviana@hotmail.com>
2021-02-22 22:40:36 -0300

    tipos de variaveis

---
+++ tipos.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt -br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exemplo</title>
+</head>
+<body>
+        <?php
+            $i =46; // Váriavel tipo inteiro
+            $s = "Gustavo"; // Váriavel tipo string
+            $r = 1.5; // Váriavel tipo real
+            $b = true; // Váriavel tipo boolean
+            echo $s. " tem ". $i. " anos" ; //metodo retorno print
+            echo "$r x 2 = 3 -2 =  $b "; // segundo método de retorno tudo dentro de uma string
+        ?>
+
+</body>
+</html>
+
+
