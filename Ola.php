5153b8567da1cdae795606799c8924763ceebef0
Your Name <catedralviana@hotmail.com>
2021-02-22 22:18:10 -0300

    Criando arquivo Print

---
+++ Ola.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exemplo</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Ola Mundo!"; // Comando que iprime na tela navegador a msg entre strings
+        ?>
+
+    </h1>
+</body>
+</html>
+
