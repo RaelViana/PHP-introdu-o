918284dbabb67b0f942b40b78215b866aa711957
RaelViana <catedralviana@hotmail.com>
2021-03-03 23:52:57 -0300

    Utilização do strlen

---
+++ String_strlen.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Strings</title>
+</head>
+<body>
+    <?php
+
+    /* Utilização do strlen (calcula o cumprimento da string) */
+    $txt = " PHP evolução do Desenvolvimento" ; // Var String texto
+    $tamanho = strlen($txt);  // Retona um INT com tamanho da String
+    echo $tamanho; // exibe o print com o Valor INT
+
+    ?>
+</body>
+</html>
