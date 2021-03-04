1257d8768047e329520dbf08b64fd7861c7530c4
RaelViana <catedralviana@hotmail.com>
2021-03-03 23:48:49 -0300

    Utilização do print_r

---
+++ String_print_r.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Strings</title>
+</head>
+<body>
+    <?php
+
+    /* Utilização do print r (vetores) */
+    $v[0] = 4; //vetor posição 0 valor 4
+    $v[1] = 7;
+    $v[2] = 6;
+
+
+   // print_r($v); // imprime os valores e indices dos vetores
+
+    $v2 = array(3,5,8,69,8,6);
+    print_r($v2); //imprime valores e indices ,mesmo quando indice não é especificado
+
+    ?>
+</body>
+</html>
