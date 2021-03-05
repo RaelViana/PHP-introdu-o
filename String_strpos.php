80a57ed9e4460ece6c276d6133996675df40a758
RaelViana <catedralviana@hotmail.com>
2021-03-04 22:30:46 -0300

    Utilização do strpos

---
+++ String_strpos.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Strings</title>
+</head>
+<body>
+    <?php
+
+    /* Utilização do strpos */
+    $frase = "Estou aprendendo PHP" ; // Var String texto
+    $pos = strpos($frase,"PHP"); // retorna um valor inteiro com a posição do texto mencionado
+    echo  "A string foi  encontrada na posição $pos";
+
+    /* A função stripos tem a a mesma função porém ignora letras mauisculas e minusculas*/
+    ?>
+</body>
+</html>S
+S
