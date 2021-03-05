d22de98caa480b47fe69017756c92f841d788bd4
RaelViana <catedralviana@hotmail.com>
2021-03-05 17:54:26 -0300

    Utilização de Matriz

---
+++ Matriz.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vetores E Matrizes</title>
+</head>
+<body>
+    <pre>
+        <?php
+            /* Matriz (Vetor de vetor)*/
+            $m = array(array(3,4), // linha 0 (coluna 0=3 , coluna 1=4)
+                       array(7,6), // linha 1 (coluna 0=7 , coluna 1=6)
+                       array(4,2)); // linha 2 (coluna 0=4 , coluna 1=2)
+            print_r($m);
+        ?>
+    </pre>
+
+</body>
+</html>
