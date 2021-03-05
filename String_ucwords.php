8490f1a12b3e2eb144977892d78e3ae489be4425
RaelViana <catedralviana@hotmail.com>
2021-03-04 22:30:01 -0300

    Utilização do ucwords

---
+++ String_ucwords.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Strings</title>
+</head>
+<body>
+    <?php
+
+    /* Utilização do ucwords */
+    $nome = "star wars " ; // Var String texto
+    print ("A palavra ficará ".ucwords($nome)); // imprime o texto com as primeiras letras das palavras Maiúsculas
+
+    ?>
+</body>
+</html>
