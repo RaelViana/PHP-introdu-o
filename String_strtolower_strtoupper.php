ea688aa29165e02bd311d031796495c8426202b3
RaelViana <catedralviana@hotmail.com>
2021-03-04 22:28:46 -0300

    Utilização do strtoupper

---
+++ String_strtolower_strtoupper.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Strings</title>
+</head>
+<body>
+    <?php
+
+    /* Utilização do strttolower */
+    $nome = "Charles Chaplin " ; // Var String texto
+    print ("Seu Nome é ".strtolower($nome)); // imprime o texto com todas as letras minusculas
+
+    /* Utilização do strttoupper */
+    $nome = "Charles Chaplin " ; // Var String texto
+    print ("<br/>Seu Nome é ".strtoupper($nome)); // imprime o texto com todas as letras maiusculas
+
+
+    ?>
+</body>
+</html>
